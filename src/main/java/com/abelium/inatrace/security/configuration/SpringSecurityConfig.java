1e1fd3f514652d744641e1043f096e72667c6d70
renan <renan.nuevo@gmail.com>
2024-01-28 18:26:45 +0800

    fix: update security config

--- src/main/java/com/abelium/inatrace/security/configuration/SpringSecurityConfig.java
+++ src/main/java/com/abelium/inatrace/security/configuration/SpringSecurityConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -22,7 +27,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
     prePostEnabled = true
 )
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -32,15 +37,28 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     public TokenAuthenticationFilter tokenAuthenticationFilter() {
         return new TokenAuthenticationFilter();
     }
-    
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://isledecoco-app.s3-website-ap-southeast-1.amazonaws.com"));
+        // config.setAllowedOrigins(Arr/ays.asList("http://127.0.0.1:8081"));
+        
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        config.setExposedHeaders(Arrays.asList("Authorization")); // Add the headers you want to expose
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
     @Bean(BeanIds.AUTHENTICATION_MANAGER)
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-		
-	
-	private static String[] swaggerExceptions = new String[] {
+
+    private static String[] swaggerExceptions = new String[] {
         "/v2/api-docs",
         "/swagger-resources",
         "/swagger-resources/**",
@@ -48,29 +66,27 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         "/configuration/security",
         "/swagger-ui.html",
         "/webjars/**"
-	};
+    };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and()
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+        http.cors().and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
             .csrf().disable()
             .formLogin().disable()
             .httpBasic().disable()
             .exceptionHandling().authenticationEntryPoint(new RestAuthenticationEntryPoint()).and()
-        	//.and().requestMatchers().antMatchers("/")
-        	.authorizeRequests()
-        		.antMatchers("/api/public/**", 
-        				"/api/user/login",
-        				"/api/user/refresh_authentication",
-        				"/api/user/register",
-        				"/api/user/request_reset_password",
-        				"/api/user/reset_password",
-        				"/api/user/confirm_email").permitAll()
-        		.antMatchers(swaggerExceptions).permitAll()
-        		.anyRequest().authenticated();		
+            .authorizeRequests()
+                .antMatchers("/api/public/**", 
+                        "/api/user/login",
+                        "/api/user/refresh_authentication",
+                        "/api/user/register",
+                        "/api/user/request_reset_password",
+                        "/api/user/reset_password",
+                        "/api/user/confirm_email").permitAll()
+                .antMatchers(swaggerExceptions).permitAll()
+                .anyRequest().authenticated();
+
         http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-
     }
-
 }
